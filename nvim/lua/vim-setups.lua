d9defbc047ee0dc9b2deaaf2f4f60838768ca856
j <joao.eudes13jem@gmail.com>
2026-05-05 23:31:08 -0300

    initial dotfiles setup

---
+++ nvim/lua/vim-setups.lua
@@ -0,0 +1,12 @@
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set number")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set textwidth=80")
+vim.cmd("set nowrap")
+vim.cmd("set formatoptions+=t")
+vim.cmd("set scrolloff=6")
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.g.mapleader = " "
